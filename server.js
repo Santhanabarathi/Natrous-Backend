c42b3965eeb7342954354694f4da1453c0074c72
Santhanabarathi <smartsanthanam1@gmail.com>
2025-09-30 18:52:21 +0530

    server.js file change

--- server.js
+++ server.js
@@ -1,53 +1,19 @@
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// dotenv.config({ path: "./.env" });
-// const app = require("./app");
-
-// mongoose.connect("mongodb://127.0.0.1:27017/natrous", {}).then(() => {
-//   console.log("DB connected succesfully");
-// });
-
-// const DB = process.env.DATABASE;
-// mongoose.connect(DB, {}).then(() => {
-//   console.log("DB connected successfull");
-// });
-
-// const port = 3002;
-
-// app.listen(port, () => {
-//   console.log(`running on port ${port}`);
-// });
-
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 const app = require("./app");
 
+mongoose.connect("mongodb://127.0.0.1:27017/natrous", {}).then(() => {
+  console.log("DB connected succesfully");
+});
+
 const DB = process.env.DATABASE;
+mongoose.connect(DB, {}).then(() => {
+  console.log("DB connected successfull");
+});
 
-// Cache connection for serverless
-let isConnected = false;
+const port = 3002;
 
-const connectDB = async () => {
-  if (isConnected && mongoose.connection.readyState === 1) {
-    console.log("Using existing database connection");
-    return;
-  }
-
-  try {
-    await mongoose.connect(DB, {
-      bufferCommands: false,
-      serverSelectionTimeoutMS: 5000,
-    });
-    isConnected = true;
-    console.log("DB connected successfully");
-  } catch (err) {
-    console.error("DB Connection Error:", err);
-  }
-};
-
-// Initialize connection
-connectDB();
-
-// Export for Vercel - NO app.listen()!
-module.exports = app;
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
