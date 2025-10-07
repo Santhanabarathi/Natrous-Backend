9e9a2f77d28d2cd5499db6ca36d2fd257d9fd1a7
Santhanabarathi <smartsanthanam1@gmail.com>
2025-10-07 17:58:56 +0530

    run time log

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ app.use("/", express.static(folder));
 // or
 // app.use("/upload", express.static("upload-Images"));
 
+app.use("/", (req, res, next) => {
+  res.status(200),
+    json({
+      status: "success",
+    });
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
