c1d7cb8108cd45e278e0803733a38bcc69ae5f9d
Santhanabarathi <smartsanthanam1@gmail.com>
2025-10-10 16:45:41 +0530

    bug

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -11,7 +11,7 @@ exports.createReview = catchAsync(async (req, res, next) => {
 
 exports.getAllReview = catchAsync(async (req, res, next) => {
   const review = await Review.find()
-    .populate("user", "name")
+    .populate("user", "name", "photo")
     .populate("tour", "name");
 
   const cleanReview = review.map((r) => {
